c48da6e3af42f63046d40ea7089cdfb04ce70ef1
PolGuixe <pol.guixe@gmail.com>
2018-09-13 16:34:47 +0200

    VulcanEmail.send now accepts an object as an input being backwards compa

--- packages/vulcan-lib/lib/modules/utils.js
+++ packages/vulcan-lib/lib/modules/utils.js
@@ -498,3 +498,20 @@ Utils.isPromise = value => isFunction(get(value, 'then'));
 Utils.pluralize = s => {
   return s.slice(-1) === 'y' ? `${s.slice(0,-1)}ies` : `${s}s`;
 }
+
+/**
+ * Handles an argument list and returns it as an object.
+ *
+ * Performs one of the following:
+ *  - returns first argument if it is an object and there are no more arguments
+ *  - keys each argument with the corresponding key in the keys array
+ *
+ * @param {String[]} keys
+ * @param {*[]} args
+ */
+Utils.argsAsObject = function argsAsObject(keys, args) {
+  if (typeof args[0] === 'object' && args.length === 1) {
+    return args[0];
+  }
+  return args.reduce((acc, arg, index) => ({ ...acc, [keys[index]]: arg }), {});
+};
